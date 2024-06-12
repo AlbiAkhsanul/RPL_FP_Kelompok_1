9dd323c12e4551dc98633fa87e976389ae78c26c
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2024-06-13 06:35:37 +0700

    ganti alamat sama benerin nama db

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
 
-define('BASEURL', 'http://localhost:90/final_project_pemweb_kel7/public');
+define('BASEURL', 'http://localhost:90/RPL_FP_Kelompok_1/public');
 
 // DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'rentacar');
+define('DB_NAME', 'travelku');
 define(' PASSWORD_DEFAULT', 'password123');
