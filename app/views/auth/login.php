6617c27bf98aaab9ff9c1742608dd97ab033c09b
KalfinSyah <kalfinsyah.km@gmail.com>
2024-06-16 20:52:42 +0700

    menghiasi

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,24 +1,22 @@
-<h1>Halaman Login</h1>
-
-<form action="<?= BASEURL; ?>/auth/authenticate" method="post">
-    <ul>
-        <li>
-            <label for="EMAIL">Email: </label>
+<div class="login_con">
+    <h1>Login</h1>
+    <form action="<?= BASEURL; ?>/auth/authenticate" method="post">
+        <div class="mail_input">
+            <label for="EMAIL">Email </label>
             <input type="text" name="EMAIL" id="EMAIL" autofocus required>
-        </li>
-        <li>
-            <label for="PASSWORD">Password: </label>
+        </div>
+
+        <div class="password_input">
+            <label for="PASSWORD">Password </label>
             <input type="password" name="PASSWORD" id="PASSWORD" autocomplete="off" required>
-        </li>
-        <li>
+        </div>
+
+        <div class="remember_me">
             <input type="checkbox" name="remember">
             <label for="remember">Ingat saya</label>
-        </li>
-        <br>
-        <li>
-            <button type="submit" name="login">Login</button>
-        </li>
-    </ul>
-</form>
+        </div>
 
+        <button type="submit" name="login">Login</button>
+    </form>
+</div>
 <a href="<?= BASEURL ?>/public/">home</a>
