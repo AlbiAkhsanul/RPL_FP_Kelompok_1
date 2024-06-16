5112919fb48e5dcbb7ab9a7d20c70ea3f3dcf43e
Irsyad Fadhil <71073281+irsyadfadhil123@users.noreply.github.com>
2024-06-16 12:57:24 +0700

    menambahkan fitur edit profil

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -67,4 +67,70 @@ class User_model
         $this->db->bind('ID_USER', $id);
         return $this->db->single();
     }
+
+    public function editUserById($data, $id) 
+    {
+        $userData = $this->getUserById($id);
+        if (!password_verify($data['PASSWORD_LAMA'], $userData['PASSWORD']))
+        {
+            echo "
+            <script>
+                alert('Password Lama tidak sesuai!');
+                window.history.go(-1);
+            </script>
+            ";    
+            exit;
+        }
+        if (!empty($data['PASSWORD_BARU']) && $data['PASSWORD_BARU'] !== $data['KONFIRMASI_PASSWORD_BARU']) 
+        {
+            echo "
+            <script>
+                alert('Konfirmasi Password tidak sesuai!');
+                window.history.go(-1);
+            </script>
+            ";    
+            exit;
+        }
+        // var_dump($this->getEmail($data['EMAIL_USER']));
+        // exit;
+        if ($userData['EMAIL_USER'] !== $data['EMAIL_USER'])
+        {
+            if ($this->getEmail($data['EMAIL_USER']))
+            {
+                echo "
+                <script>
+                    alert('Email Telah Dipakai, Coba Email Lain!');
+                    window.history.go(-1);
+                </script>
+                ";    
+                exit;
+            }
+        }        
+        $password = $data['PASSWORD_BARU'] === '' ? $userData['PASSWORD'] : password_hash($data['PASSWORD_BARU'], PASSWORD_DEFAULT);
+        $query = "UPDATE {$this->table_name} SET 
+        NAMA = :NAMA, 
+        NO_TELP = :NO_TELP, 
+        EMAIL_USER = :EMAIL_USER, 
+        PASSWORD = :PASSWORD 
+        WHERE ID_USER = :ID_USER";
+
+        $this->db->query($query);
+        $this->db->bind('NAMA', $data['NAMA']);
+        $this->db->bind('NO_TELP', $data['NO_TELP']);
+        $this->db->bind('EMAIL_USER', $data['EMAIL_USER']);
+        $this->db->bind('PASSWORD', $password);
+        $this->db->bind('ID_USER', $id);
+        $this->db->execute();
+
+        return $this->db->affectedRowCount();
+    }
+
+    public function getEmail($email)
+    {
+        $query = "SELECT EMAIL_USER FROM {$this->table_name} WHERE EMAIL_USER = :EMAIL_USER";
+        $this->db->query($query);
+        $this->db->bind('EMAIL_USER', $email);
+
+        return $this->db->single();
+    }
 }
