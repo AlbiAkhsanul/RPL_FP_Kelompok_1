37237c81f4c949bcec0557b84342948491b661d3
mikhail-karim <mikhailsakarim@gmail.com>
2024-06-23 23:46:57 +0700

    bootstrap form

--- app/views/order/create.php
+++ app/views/order/create.php
@@ -5,49 +5,53 @@ $rute = $data['rute'];
 $methods = $data['transaksi'];
 ?>
 
-<ul>
-    <li>
-        Rute Perjalanan : <?= $rute['NAMA_RUTE'] ?>
-    </li>
-    <li>
-        Tanggal Perjalanan : <?= $order_rute['TANGGAL_PERJALANAN'] ?>
-    </li>
-    <li>
-        Kapasitas Penumpang : <?= $order_rute['JUMLAH_PENUMPANG'] ?>
-    </li>
-    <li>
-        Mobil : [<?= $car['NOPOL'] ?>] <?= $car['JENIS_MOBIL'] ?> <?= $car['MERK'] ?>
-    </li>
-    <li>
-        Harga : <?= $order_rute['TOTAL_HARGA'] ?>
-    </li>
-</ul>
-
-<form action="<?= BASEURL; ?>/order/store/<?= $order_rute['ID_PESANAN_RUTE'] ?>" method="post">
-    <input type="hidden" name="ID_PESANAN_RUTE" value="<?= $order_rute['ID_PESANAN_RUTE']; ?>">
-    <input type="hidden" name="ID_USER" value="<?= $_SESSION['user_id']; ?>">
-    <input type="hidden" name="ID_RUTE" value="<?= $rute['ID_RUTE']; ?>">
-    <input type="hidden" name="TANGGAL_PERJALANAN" value="<?= $order_rute['TANGGAL_PERJALANAN']; ?>">
-    <input type="hidden" name="TOTAL_HARGA" value="<?= $order_rute['TOTAL_HARGA']; ?>">
-    <ul>
-        <li>
-            <label for="ALAMAT_PENJEMPUTAN">Alamat Penjemputan: </label>
-            <textarea name="ALAMAT_PENJEMPUTAN" id="ALAMAT_PENJEMPUTAN" rows="2" cols="40"></textarea>
-        </li>
-        <li>
-            <label for="ALAMAT_PENURUNAN">Alamat Penurunan: </label>
-            <textarea name="ALAMAT_PENURUNAN" id="ALAMAT_PENURUNAN" rows="2" cols="40"></textarea>
-        </li>
-        <li>
-            <label for=" ID_METODE">Metode Pembayaran: </label>
-            <select id="ID_METODE" name="ID_METODE" required>
-                <?php foreach ($methods as $method) : ?>
-                    <option value="<?= $method['ID_METODE'] ?>"><?= $method['NAMA_METODE'] ?></option>
-                <?php endforeach; ?>
-            </select>
-        </li>
-        <li>
-            <button type="submit" name="store">Buat Pesanan User</button>
-        </li>
-    </ul>
-</form>
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-header">
+            <h4>Order Details</h4>
+        </div>
+        <div class="card-body">
+            <ul class="list-group mb-4">
+                <li class="list-group-item">
+                    <strong>Perjalanan : </strong><?= $rute['NAMA_RUTE'] ?>
+                </li class="list-group-item">
+                <li class="list-group-item">
+                    <strong>Tanggal Perjalanan : </strong><?= $order_rute['TANGGAL_PERJALANAN'] ?>
+                </li class="list-group-item">
+                <li class="list-group-item">
+                    <strong>Kapasitas Penumpang : </strong><?= $order_rute['JUMLAH_PENUMPANG'] ?>
+                </li class="list-group-item">
+                <li class="list-group-item">
+                    <strong>Mobil : </strong>[<?= $car['NOPOL'] ?>] <?= $car['JENIS_MOBIL'] ?> <?= $car['MERK'] ?>
+                </li class="list-group-item">
+                <li class="list-group-item">
+                    <strong>Harga : </strong><?= $order_rute['TOTAL_HARGA'] ?>
+                </li class="list-group-item">
+            </ul>
+            <form action="<?= BASEURL; ?>/order/store/<?= $order_rute['ID_PESANAN_RUTE'] ?>" method="post">
+            <input type="hidden" name="ID_PESANAN_RUTE" value="<?= $order_rute['ID_PESANAN_RUTE']; ?>">
+            <input type="hidden" name="ID_USER" value="<?= $_SESSION['user_id']; ?>">
+            <input type="hidden" name="ID_RUTE" value="<?= $rute['ID_RUTE']; ?>">
+            <input type="hidden" name="TANGGAL_PERJALANAN" value="<?= $order_rute['TANGGAL_PERJALANAN']; ?>">
+            <input type="hidden" name="TOTAL_HARGA" value="<?= $order_rute['TOTAL_HARGA']; ?>">
+            <div class="mb-3">
+                <label for="ALAMAT_PENJEMPUTAN" class="form-label">Alamat Penjemputan: </label>
+                <textarea name="ALAMAT_PENJEMPUTAN" id="ALAMAT_PENJEMPUTAN" class="form-control" rows="2" cols="40"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="ALAMAT_PENURUNAN" class="form-label">Alamat Penurunan: </label>
+                <textarea name="ALAMAT_PENURUNAN" id="ALAMAT_PENURUNAN" class="form-control" rows="2" cols="40"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for=" ID_METODE" class="form-label">Metode Pembayaran: </label>
+                <select id="ID_METODE" name="ID_METODE" class="form-select" required>
+                    <?php foreach ($methods as $method) : ?>
+                        <option value="<?= $method['ID_METODE'] ?>"><?= $method['NAMA_METODE'] ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+                <button type="submit" name="store" class="btn btn-primary">Buat Pesanan User</button>
+            </form>
+        </div>
+    </div>
+</div>
