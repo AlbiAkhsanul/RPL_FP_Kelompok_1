bb3cbd8a1fb8062a2ee6095ffe5e6af4e2517f55
KalfinSyah <kalfinsyah.km@gmail.com>
2024-06-23 16:26:05 +0700

    tambahin logout

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,12 +1,16 @@
 <a href="<?= BASEURL ?>/auth/login">login</a>
 <a href="<?= BASEURL ?>/auth/register">register</a>
+<?= isset($_SESSION['user_id']) ? "<a href='" . BASEURL . "/auth/logout'>logout</a>" : '' ?>
+
+
+
 <div class="container mt-3">
     <div class="jumbotron">
         <h1 class="display-4">Halaman Utama</h1>
         <p class="lead">
         <h3>Ini Halaman Awal</h3>
         <h1>
-            Halo <?= isset($_SESSION['user_id']) ? $data['user']['USERNAME'] : '' ?> !
+            Halo <?= isset($_SESSION['user_id']) ? $data['user']['NAMA'] : '' ?> !
         </h1>
     </div>
 </div>
