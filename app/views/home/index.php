2265f2705d3bb354bae92e4ef2f421ba44e079fd
KalfinSyah <kalfinsyah.km@gmail.com>
2024-06-23 19:08:55 +0700

    tambah navbar dan edit yg lain aku lupa cik

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,15 +1,18 @@
-<?php if (!isset($_SESSION['user_id'])) : ?>
-    <a href="<?= BASEURL ?>/auth/login">login</a>
-    <a href="<?= BASEURL ?>/auth/register">register</a>
-<?php else : ?>
-    <h1>Welcome <?= $data['user']['NAMA'] ?></h1>
-    <a href='<?= BASEURL ?>/auth/logout'>logout</a>
-<?php endif; ?>
-
-<div class="container mt-3">
+<div class="container nologin">
     <div class="jumbotron">
-        <h1 class="display-4">Halaman Utama</h1>
-        <p class="lead">
-        <h3>Ini Halaman Awal</h3>
+        <?php if (!isset($_SESSION['user_id'])) : ?>
+            <h1 class="display-4 fw-bold">Judul Website</h1>
+            <h3 class="fw-light">Website pelayanan travel murah dari Indonesia.</h3>
+        <?php else : ?>
+            <h1 class="display-4 fw-bold shadow-sm">Selamat Datang</h1>
+            <h3 class="fw-light shadow-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit suscipit esse ab ea ut et? Tempore sed dolores nobis quidem. Explicabo ducimus fugiat, laudantium quam quisquam ipsam minus quod nulla.</h3>
+        <?php endif; ?>
+    </div>
+
+    <div class="nonlogin_login_reg">
+        <?php if (!isset($_SESSION['user_id'])) : ?>
+            <a class="btn btn-primary shadow-sm" href="<?= BASEURL ?>/auth/login" role="button">Login</a>
+            <a class="btn btn-primary shadow-sm" href="<?= BASEURL ?>/auth/register" role="button">Register</a>
+        <?php endif; ?>
     </div>
 </div>
