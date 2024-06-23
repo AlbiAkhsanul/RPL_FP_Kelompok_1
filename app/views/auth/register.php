eb92f9f0eb340aa5596ecc5abee926658a7a44ee
KalfinSyah <kalfinsyah.km@gmail.com>
2024-06-23 21:13:21 +0700

    ganti bg

--- app/views/auth/register.php
+++ app/views/auth/register.php
@@ -1,44 +1,47 @@
-<div class="auth_con">
-    <form class="shadow" action="<?= BASEURL; ?>/auth/store" method="post">
-            <div class="auth_title">
-                <h1>REGISTER</h1>
-            </div>
+<div class="homecont">
+    <img src="<?= BASEURL ?>/img/frontend/car_travels_bg.jpg" alt="">
+    <div class="auth_con">
+        <form class="shadow" action="<?= BASEURL; ?>/auth/store" method="post">
+                <div class="auth_title">
+                    <h1>REGISTER</h1>
+                </div>
 
-            <div class="basic_auth_field">
-                <label for="NAMA">Nama</label>
-                <input type="text" name="NAMA" id="NAMA" autocomplete="off" autofocus required>
-            </div>
+                <div class="basic_auth_field">
+                    <label for="NAMA">Nama</label>
+                    <input type="text" name="NAMA" id="NAMA" autocomplete="off" autofocus required>
+                </div>
 
-            <div class="basic_auth_field">
-                <label for="EMAIL_USER">Email</label>
-                <input type="text" name="EMAIL_USER" id="EMAIL_USER" autocomplete="off" autofocus required>
-            </div>
+                <div class="basic_auth_field">
+                    <label for="EMAIL_USER">Email</label>
+                    <input type="text" name="EMAIL_USER" id="EMAIL_USER" autocomplete="off" autofocus required>
+                </div>
 
-            <div class="basic_auth_field">
-                <label for="PASSWORD">Password</label>
-                <input type="password" name="PASSWORD" id="PASSWORD" autocomplete="off" required>
-            </div>
+                <div class="basic_auth_field">
+                    <label for="PASSWORD">Password</label>
+                    <input type="password" name="PASSWORD" id="PASSWORD" autocomplete="off" required>
+                </div>
 
-            <div class="basic_auth_field">
-                <label for="PASSWORD_CONFIRM">Konfirmasi Password</label>
-                <input type="password" name="PASSWORD_CONFIRM" id="PASSWORD_CONFIRM" autocomplete="off" required>
-            </div>
+                <div class="basic_auth_field">
+                    <label for="PASSWORD_CONFIRM">Konfirmasi Password</label>
+                    <input type="password" name="PASSWORD_CONFIRM" id="PASSWORD_CONFIRM" autocomplete="off" required>
+                </div>
 
-            <div class="basic_auth_field">
-                <label for="NO_TELP">No Telp</label>
-                <input type="number" name="NO_TELP" id="NO_TELP" autocomplete="off" required>
-            </div>
+                <div class="basic_auth_field">
+                    <label for="NO_TELP">No Telp</label>
+                    <input type="number" name="NO_TELP" id="NO_TELP" autocomplete="off" required>
+                </div>
 
-            <div class="sdhpnyakn">
-                <a href="<?= BASEURL ?>/auth/login">Sudah mempunyai akun?</a>
-            </div>
-            
-            <div class="submit_btn">
-                <button type="submit" name="SignUp">Sign Up</button>
-            </div>
-    </form>
+                <div class="sdhpnyakn">
+                    <a href="<?= BASEURL ?>/auth/login">Sudah mempunyai akun?</a>
+                </div>
+                
+                <div class="submit_btn">
+                    <button type="submit" name="SignUp">Sign Up</button>
+                </div>
+        </form>
 
-    <div class="backhome">
-        <a class="btn btn-primary shadow" href="<?= BASEURL ?>" role="button">Back to Home</a>
+        <div class="backhome">
+            <a class="btn btn-primary shadow" href="<?= BASEURL ?>" role="button">Back to Home</a>
+        </div>
     </div>
 </div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,18 +1,23 @@
-<div class="container nologin">
-    <div class="jumbotron">
-        <?php if (!isset($_SESSION['user_id'])) : ?>
-            <h1 class="display-4 fw-bold">Judul Website</h1>
-            <h3 class="fw-light">Website pelayanan travel murah dari Indonesia.</h3>
-        <?php else : ?>
-            <h1 class="display-4 fw-bold">Selamat Datang</h1>
-            <h3 class="fw-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit suscipit esse ab ea ut et? Tempore sed dolores nobis quidem. Explicabo ducimus fugiat, laudantium quam quisquam ipsam minus quod nulla.</h3>
-        <?php endif; ?>
-    </div>
+<div class="homecont">
+    <?php if (!isset($_SESSION['user_id'])) : ?>
+        <img src="<?= BASEURL ?>/img/frontend/car_travels_bg.jpg" alt="">
+    <?php endif; ?>
+    <div class="container nologin">
+        <div class="jumbotron">
+            <?php if (!isset($_SESSION['user_id'])) : ?>
+                <h1 class="display-4 fw-bold text-light">Travelku</h1>
+                <h3 class="fw-light text-light">Website pelayanan travel murah dari Indonesia.</h3>
+            <?php else : ?>
+                <h1 class="display-4 fw-bold">Selamat Datang</h1>
+                <h3 class="fw-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit suscipit esse ab ea ut et? Tempore sed dolores nobis quidem. Explicabo ducimus fugiat, laudantium quam quisquam ipsam minus quod nulla.</h3>
+            <?php endif; ?>
+        </div>
 
-    <div class="nonlogin_login_reg">
-        <?php if (!isset($_SESSION['user_id'])) : ?>
-            <a class="btn btn-primary shadow-sm" href="<?= BASEURL ?>/auth/login" role="button">Login</a>
-            <a class="btn btn-primary shadow-sm" href="<?= BASEURL ?>/auth/register" role="button">Register</a>
-        <?php endif; ?>
+        <div class="nonlogin_login_reg">
+            <?php if (!isset($_SESSION['user_id'])) : ?>
+                <a class="btn btn-primary shadow-sm" href="<?= BASEURL ?>/auth/login" role="button">Login</a>
+                <a class="btn btn-primary shadow-sm" href="<?= BASEURL ?>/auth/register" role="button">Register</a>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
