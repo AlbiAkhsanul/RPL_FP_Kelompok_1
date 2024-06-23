07c2c5a883a13b4920d64d0399d1b900039c5f0d
KalfinSyah <kalfinsyah.km@gmail.com>
2024-06-23 19:10:35 +0700

    salah desain cik

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,8 +4,8 @@
             <h1 class="display-4 fw-bold">Judul Website</h1>
             <h3 class="fw-light">Website pelayanan travel murah dari Indonesia.</h3>
         <?php else : ?>
-            <h1 class="display-4 fw-bold shadow-sm">Selamat Datang</h1>
-            <h3 class="fw-light shadow-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit suscipit esse ab ea ut et? Tempore sed dolores nobis quidem. Explicabo ducimus fugiat, laudantium quam quisquam ipsam minus quod nulla.</h3>
+            <h1 class="display-4 fw-bold">Selamat Datang</h1>
+            <h3 class="fw-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit suscipit esse ab ea ut et? Tempore sed dolores nobis quidem. Explicabo ducimus fugiat, laudantium quam quisquam ipsam minus quod nulla.</h3>
         <?php endif; ?>
     </div>
 
