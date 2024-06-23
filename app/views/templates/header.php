fb44f3fbb1ab9dc8d8bdff7d1a971122a3727738
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2024-06-23 23:44:08 +0700

    benerin button header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -20,13 +20,13 @@
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>">Home</a>
+              <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>/home">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>">Travels</a>
+              <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>/orderRute/index">Travels</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>">My Orders</a>
+              <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>/order/index">My Orders</a>
             </li>
           </ul>
           <div class="btn-group">
