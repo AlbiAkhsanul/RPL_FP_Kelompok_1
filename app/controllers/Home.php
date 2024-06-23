cda35361320750d514b2ef7e66bcc8731c3de21a
Albi Akhsanul H <albiakhsanulhakim24@gmail.com>
2024-06-23 19:47:04 +0700

    benerin

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,9 +4,6 @@ class Home extends Controller
 {
     public function index()
     {
-        if (isset($_SESSION['user_id'])) {
-            $data['user'] = $this->model('User_model')->getUserById($_SESSION['user_id']);
-        }
         $data['title'] = 'Home';
         $this->view('templates/header', $data);
         $this->view('home/index', $data);
