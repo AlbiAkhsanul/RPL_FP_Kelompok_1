efb1f29c468c8b091c3e6b98ef8722165c9c5b92
Irsyad Fadhil <71073281+irsyadfadhil123@users.noreply.github.com>
2024-07-03 17:04:12 +0700

    memperbaiki frontend mobil

--- app/views/admin/drivers/edit.php
+++ app/views/admin/drivers/edit.php
@@ -1,21 +1,19 @@
 <?php
 $driver = $data['driver'];
 ?>
-        <a href="<?= BASEURL ?>/admin/drivers/index" class="btn btn-secondary mt-3 ml-3">
-            Kembali
-        </a>
-    <div class="container mt-5">
-        <h1 class="mb-4">Edit Driver</h1>
+<a href="<?= BASEURL ?>/admin/drivers/index" class="btn btn-secondary mt-3 ml-3">Kembali</a>
+<div class="container mt-5">
+    <h1 class="mb-4">Edit Driver</h1>
 
-        <form action="<?= BASEURL; ?>/driver/update/<?= $driver['ID_SUPIR'] ?>" method="post">
-            <div class="form-group">
-                <label for="NAMA">Nama Driver: </label>
-                <input type="text" class="form-control" name="NAMA" id="NAMA" value="<?= $driver['NAMA']; ?>" autofocus required>
-            </div>
-            <div class="form-group">
-                <label for="NO_TELP">No Telepon Driver: </label>
-                <input type="number" class="form-control" name="NO_TELP" id="NO_TELP" value="<?= $driver['NO_TELP']; ?>" required>
-            </div>
-            <button type="submit" name="store" class="btn btn-primary mt-3">Update Driver</button>
-        </form>
-    </div>
+    <form action="<?= BASEURL; ?>/driver/update/<?= $driver['ID_SUPIR'] ?>" method="post">
+        <div class="form-group">
+            <label for="NAMA">Nama Driver: </label>
+            <input type="text" class="form-control" name="NAMA" id="NAMA" value="<?= $driver['NAMA']; ?>" autofocus required>
+        </div>
+        <div class="form-group">
+            <label for="NO_TELP">No Telepon Driver: </label>
+            <input type="number" class="form-control" name="NO_TELP" id="NO_TELP" value="<?= $driver['NO_TELP']; ?>" required>
+        </div>
+        <button type="submit" name="store" class="btn btn-primary mt-3">Update Driver</button>
+    </form>
+</div>
